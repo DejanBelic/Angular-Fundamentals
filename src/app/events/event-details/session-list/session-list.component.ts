e09155abaf2d714d038807c5ccb51c9948ff5c4b
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2020-04-23 19:16:44 +0200

    Filtering Data.

--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {ISession} from '../../shared';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,12 +7,26 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.scss']
 })
-export class SessionListComponent implements OnInit {
-@Input() sessions: ISession;
+export class SessionListComponent implements OnChanges {
+@Input() sessions: ISession[];
+@Input() filterBy: string;
+  visibleSessions: ISession[] = [];
   faCoffee = faCoffee;
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+    this.visibleSessions = this.sessions.filter(session => {
+      return session.level.toLocaleLowerCase() === filter;
+    });
+    }
   }
 
 }
